d1a047027508dd0073761ff372c5b2c1896236cb
tcostello74 <tcostello74@yahoo.com>
2023-10-07 12:04:32 -0400

    Carousel-tests

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import Carousel from "./Carousel";
 
 it("works when you click on the right arrow", function() {
@@ -16,4 +18,19 @@ it("works when you click on the right arrow", function() {
   // expect the second image to show, but not the first
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+});
+
+// smoke test
+it('Carousel renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Carousel />, div); // Using defaultProps for simplicity
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+// snapshot test
+it('Carousel renders correctly according to snapshot', () => {
+  const tree = renderer
+    .create(<Carousel />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
